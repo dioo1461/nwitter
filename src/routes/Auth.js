08fee3d57bae7efaf043e79fc2c9b31686c70f7d
dioo1461 <dioo1461@naver.com>
2022-08-11 18:06:01 +0900

    220811

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,10 +1,12 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [isNewAccount, setIsNewAccount] = useState(false);
     const onChange = (event) => {
-        const {target: {name, value}} = event;
+        const { target: { name, value } } = event;
         if (name === "email") {
             setEmail(value);
         } else if (name === "password") {
@@ -13,18 +15,36 @@ const Auth = () => {
     }
     const onSubmit = (event) => {
         event.preventDefault();
+        if (isNewAccount) {
+            const auth = getAuth();
+            createUserWithEmailAndPassword(auth, email, password)
+                .then((userCredential) => {
+                    // Signed in
+                    const user = userCredential.user;
+                    // ...
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    // ..
+                });
+        } else {
+
+        }
     }
-    return (<div>
-        <form onSubmit={onSubmit}>
-            <input name="email" type="text" placeholder="Email" required value={email} onChange={onChange} />
-            <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange} />
-            <input type="submit" value="Log In" />
-        </form>
+    return (
         <div>
-            <button>Continue with Google</button>
-            <button>Continue with Github</button>
+            <form onSubmit={onSubmit}>
+                <input name="email" type="text" placeholder="Email" required value={email} onChange={onChange} />
+                <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange} />
+                <input type="submit" value="Log In" />
+                <input type="submit" value={isNewAccount ? "Create Account" : "log in"} />
+            </form>
+            <div>
+                <button>Continue with Google</button>
+                <button>Continue with Github</button>
+            </div>
         </div>
-    </div>
     );
 }
 export default Auth;
